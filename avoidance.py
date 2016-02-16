bf466b5dd1583e5c270f518ad41c31c622d567d7
Dong-Gil Shin <dgshin@mit.edu>
2016-02-16 18:36:41 -0500

    added method to compute degrees

--- avoidance.py
+++ avoidance.py
@@ -6,21 +6,58 @@ def run(n,pi):
     print x
     return x
 
+def analyze_tree(data):
+    allDegrees = {}
+    for i in data:
+        for j in i:
+            origPerm = j[0]
+            addPerms = j[1]
+            parentDegrees = []
+
+            origTuple = tuple(origPerm)
+            
+            if len(origPerm)>0:
+                maxIndex = 0
+                maxValue = 0
+                for k in range(len(origPerm)):
+                    if origPerm[k]>maxValue:
+                        maxValue = origPerm[k]
+                        maxIndex = k
+
+                prevPerm = origPerm[:maxIndex] + origPerm[maxIndex+1:]
+                prefixTuple = tuple(prevPerm)
+                print prefixTuple
+                parentDegrees = allDegrees[prefixTuple]
+                print parentDegrees
+                
+            allDegrees[origTuple] = list(parentDegrees) + [len(addPerms)]
+
+    return allDegrees
+            
+
 def generate_data_tree(n,pi):
-    data = [[ [None, [1]] ]]
+    data = [[    [[],[[1]]]   ]] # [[    [None,[[1]]]   ]]
     for i in range(n-1):
         print i
         newlevel = []
-        for x1 in data[-1]:
-            for x in x1[1]:
-                
-                newset = []
-                for j in range(i+2):
-                    k = x[:j] + [i+2] + x[j:]
-                    print k
-                    if check_avoid(k,pi):
-                        newset.append(k)
-                newlevel.append([[x],newset])
+        
+        prevlevel = []
+
+        for y in data[-1]:
+            prevlevel = prevlevel + y[1]
+
+
+        for x in prevlevel:
+
+            newset = []
+            for j in range(i+2):
+                k=x[:j] + [i+2] + x[j:]
+                if check_avoid(k,pi):
+                    newset.append(k)
+
+            newlevel.append([x, newset])
+        
+
         data.append(newlevel)
     return data
 
